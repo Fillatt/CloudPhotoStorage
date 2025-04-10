d2a52771cbabd28304cf1459b403564fab99319f
Papizzi ᠌ <ivanbelunicev61@gmail.com>
2025-04-10 20:56:39 +0300

    Добавление моделей Category, Image, WasteBasket

---
+++ CloudPhotoStorage.DataBase/Models/Image.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudPhotoStorage.DataBase.Models
+{
+    public class Image
+    {
+        [Key]
+        public int ImageId {  get; set; }
+
+        [ForeignKey("UserId")]
+        public int UserId { get; set; }
+
+        [ForeignKey("CategoryId")]
+        public int CategoryId {  get; set; }
+
+        public string? FileName {  get; set; }
+        public string? FilePath {  get; set; }
+        public DateTime? UploadDate { get; set; }
+    }
+}
---
+++ CloudPhotoStorage.DataBase/Models/WasteBasket.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudPhotoStorage.DataBase.Models
+{
+    public class WasteBasket
+    {
+        [Key]
+        public int WasteBasketId { get; set; }
+
+        [ForeignKey("UserId")]
+        public int UserId { get; set; }
+
+        [ForeignKey("ImageId")]
+        public int ImageId { get; set; }
+
+        public DateTime? DeleteDate { get; set; }
+    }
+}
