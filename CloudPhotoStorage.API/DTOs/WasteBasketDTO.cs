7c43660b85bdd1e2cd84a4d76a5169e2efeb2400
MaxStar133 <ov4innickovmax@yandex.ru>
2025-04-11 22:30:52 +0300

    Добавлен: ImageController, ImageDTO, WasteBasketDTO

---
+++ CloudPhotoStorage.API/DTOs/WasteBasketDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace CloudPhotoStorage.API.DTOs
+{
+    public class WasteBasketDTO
+    {
+        public string FileName { get; set; }
+        public string UserLogin { get; set; }
+        public DateTime? DeleteDate { get; set; }
+    }
+}
