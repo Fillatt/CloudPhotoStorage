7199818daf476fa1ccb318c2ac9d55f238178ab7
Papizzi <ivanbelunicev61@gmail.com>
2025-04-11 14:11:44 +0300

    Удаление ненужных библиотек и добавление атрибутов ForeignKey и Key для модели LoginHistory

--- CloudPhotoStorage.DataBase/Models/LoginHistory.cs
+++ CloudPhotoStorage.DataBase/Models/LoginHistory.cs
@@ -1,17 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CloudPhotoStorage.DataBase.Models
 {
     public class LoginHistory
     {
-        // Первичный ключ
+        [Key]
         public int LoginId { get; set; }
-        // Внешний ключ
+        
+        [ForeignKey("UserId")]
         public int UserId { get; set; }
+
         public DateTime LoginDate { get; set; }
     }
 }
--- CloudPhotoStorage.DataBase/Models/User.cs
+++ CloudPhotoStorage.DataBase/Models/User.cs
@@ -1,23 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CloudPhotoStorage.DataBase.Models
 {
     public class User
     {
-        // Первичный ключ
         [Key]
         public int UserId { get; set; }
+
+        [ForeignKey("RoleID")]
+        public string RoleID { get; set; }
+
         public string Login { get; set; }
         public string PasswordHash { get; set; }
         public string PasswordSalt { get; set; }
-        // Внешний ключ
-        [ForeignKey("RoleID")]
-        public string RoleID { get; set; }
     }
 }
