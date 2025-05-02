4892eccf6e5495ff02ccb9aae17f71476922b0d1
Denis <136975064+Jden10@users.noreply.github.com>
2025-05-02 20:34:40 +0300

    LogiView

--- CloudPhotoStorage.UI/ViewModels/LoginViewModel.cs
+++ CloudPhotoStorage.UI/ViewModels/LoginViewModel.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using ReactiveUI;
 
 namespace CloudPhotoStorage.UI.ViewModels;
 
 public class LoginViewModel : ViewModelBase
 {
+    public string Login { get; set; }
+    public string Password { get; set; }
 
+    public ICommand LoginCommand { get; }
+    public ICommand RegisterCommand { get; }
+
+    public LoginViewModel()
+    {
+        LoginCommand = ReactiveCommand.Create(OnLogin);
+        RegisterCommand = ReactiveCommand.Create(OnRegister);
+    }
+
+    private void OnLogin()
+    {
+        // Авторизация
+    }
+
+    private void OnRegister()
+    {
+        // Переход к регистрации
+    }
 }
