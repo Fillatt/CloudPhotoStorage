2c707c5a3bf7158c0278475635a4e91079fee0d8
MaxStar133 <ov4innickovmax@yandex.ru>
2025-05-28 21:37:54 +0300

    changed GetImageNamesWithCategories

---
+++ CloudPhotoStorage.API/DTOs/ImageWithCategoryDto.cs
@@ -0,0 +1,8 @@
+﻿namespace CloudPhotoStorage.API.DTOs
+{
+    public record ImageWithCategoryDto
+    {
+        public required string ImageName { get; set; }
+        public required string Category { get; set; }
+    }
+}
