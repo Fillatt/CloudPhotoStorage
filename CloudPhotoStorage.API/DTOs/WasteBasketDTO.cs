9ec2e333f79e54e196961fe90c84607f692fa6bb
Papizzi ᠌ <ivanbelunicev61@gmail.com>
2025-06-04 21:53:47 +0300

    delete modules

--- CloudPhotoStorage.API/DTOs/WasteBasketDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CloudPhotoStorage.API.DTOs
-{
-    public class WasteBasketDTO
-    {
-        public required string FileName { get; set; }
-        public required string UserLogin { get; set; }
-        public required DateTime? DeleteDate { get; set; }
-    }
-}
